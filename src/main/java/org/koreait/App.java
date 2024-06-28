81eaac5fdeba0046b2b3128eabb83bf9f3b91ecf
eunseo1030 <eunseo20001030@gmail.com>
2024-06-28 14:58:08 +0900

    Container를 ë도입해서 구조개선

---
+++ src/main/java/org/koreait/App.java
@@ -0,0 +1,44 @@
+package org.koreait;
+import org.koreait.motivation.controller.MotivationController;
+import org.koreait.system.controller.SystemController;
+
+
+public class App {
+
+
+    public void run() {
+        System.out.println("== motivation execution ==");
+
+        SystemController systemController = new SystemController();
+        MotivationController motivationController = new MotivationController();
+
+        while (true) {
+            System.out.print("command) ");
+            String cmd = Container.getScanner().nextLine().trim();
+
+            if (cmd.equals("exit")) {
+                systemController.exit();
+                break;
+            } else if (cmd.length() == 0) {
+                System.out.println("명령어 입력해");
+                continue;
+            }
+            if (cmd.equals("add")) {
+                motivationController.add();
+            } else if (cmd.equals("list")) {
+                motivationController.list();
+            }
+
+            if(cmd.equals("삭제?")) {
+
+            }
+
+
+
+
+
+            }
+        }
+    }
+
+
