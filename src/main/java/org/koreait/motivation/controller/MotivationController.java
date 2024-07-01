06d36d40962234083d1bf6fc5447bf18a76d5aff
eunseo1030 <eunseo20001030@gmail.com>
2024-07-01 12:43:11 +0900

    삭제 기능 구현, 없는 motivation에 대한 처리, value가 정수 형태가 아닐 경우 처리

--- src/main/java/org/koreait/motivation/controller/MotivationController.java
+++ src/main/java/org/koreait/motivation/controller/MotivationController.java
@@ -1,30 +1,25 @@
 package org.koreait.motivation.controller;
 
 import org.koreait.Container;
+import org.koreait.Rq;
 import org.koreait.motivation.entity.Motivation;
 
 import java.util.ArrayList;
 import java.util.List;
-
 public class MotivationController {
-
     int lastId;
     List<Motivation> motivations;
-
     public MotivationController() {
         lastId = 0;
         motivations = new ArrayList<>();
     }
-
     public void add() {
         int id = lastId + 1;
         System.out.print("body : ");
         String body = Container.getScanner().nextLine();
         System.out.print("source : ");
         String source = Container.getScanner().nextLine();
-
         Motivation motivation = new Motivation(id, body, source);
-
         motivations.add(motivation);
         System.out.printf("%d번 motivation이 등록 되었습니다\n", id);
         lastId++; // 마지막 번호 증가
@@ -45,5 +40,38 @@ public class MotivationController {
             }
             System.out.printf("   %d  //    %s     //    %s  \n", motivation.getId(), motivation.getSource(), motivation.getBody());
         }
+    }
+
+    public void delete(Rq rq) {
+        System.out.println("delete 실행");
+
+        int id;
+
+        try {
+            id = Integer.parseInt(rq.getParams("id"));
+        } catch (NumberFormatException e) {
+            System.out.println("정수 입력 오류");
+            return;
+        }
+
+        Motivation motivation = findById(id);
+
+        if (motivation == null) {
+            System.out.printf("%d번 motivation은 없어\n", id);
+            return;
+        }
+
+        motivations.remove(motivation);
+        System.out.printf("%d번 motivation을 삭제했습니다\n", id);
+    }
+
+    private Motivation findById(int id) {
+        for (Motivation motivation : motivations) {
+            if (motivation.getId() == id) {
+                return motivation;
+            }
+        }
+        return null;
+
     }
 }
